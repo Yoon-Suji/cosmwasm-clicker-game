6d0418b97b87a9e96bdb60f71efc4314d3164ac7
Yoon-Suji <jidl88@ewhain.net>
2022-06-15 16:32:22 +0900

    [1-1] connect testnet network to keplr wallet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import { useState } from 'react';
+import networkInfo from './wallet/network_info';
+import connectWallet from './wallet/connect';
 
 function App() {
+  // connectWallet에서 받아올 값
+  const [client, setClient] = useState();
+  const [address, setAddress] = useState();
+  const [balance, setBalance] = useState();
+  const [chainId, setChainId] = useState("");
+
+  // 
+  const getInfo = (client, address, balance, chainId) => {
+    setClient(client);
+    setAddress(address);
+    setBalance(balance);
+    setChainId(chainId);
+    console.log(address);
+  }
+
+  connectWallet(networkInfo['cliffnet-1'], {getInfo});
+
   return (
     <div className="App">
-
     </div>
   );
 }
