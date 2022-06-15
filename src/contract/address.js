f5716dd9dd57722924f36a890466839c0ac94142
Yoon-Suji <jidl88@ewhain.net>
2022-06-15 12:24:17 +0900

    init

---
+++ src/contract/address.js
@@ -0,0 +1,8 @@
+const contractAddress = {
+    "cliffnet-1" : "wasm1chnsdmv64xf3p6e7lj8lq2lhkfxgkwjfx4gujt0u3rcdxyu8f8ds0j9ktd",
+    "osmo-test-4" : "osmo1sm8weyvz7ues2mx9eg6rnqu9yazjdwru5p6u7u0jkhgmk6vqt8equ8t5xp",
+    "uni-3" : "juno1yfp9zyx9zhqe77d05yqjx3ctqjhzha0xn5d9x8zxcpp658ks2hvqlfjt72",
+    "constantine-1" : "archway1wnuakyjhvlnepk2g9ncvvaks0zy0axgx70pet4jh2nv8lmsuff9qseuvpc"
+};
+
+export default contractAddress;
