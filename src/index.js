f4e898a26a2cca27159b2bf737083a7a69b5fd31
Yoon-Suji <jidl88@ewhain.net>
2022-06-15 16:28:37 +0900

    init

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='App-header'>
+      <App />
       <div className='footer-container'>
         <span>Made with</span> 
         <img src='./dsrv.png' id='footer-logo' alt='dsrv-logo'></img>
